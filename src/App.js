19dd6d6f4b5c8c18d830d1ee124df4d5bc1e3a95
stefzouille <contact.lokcy@gmail.com>
2021-04-27 10:21:59 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Todos from "./components/Todos"
+import { useState, useEffect } from "react"
+import DarkMode from ("./components/DarkMode")
 
 function App() {
+  const [dark, setDark] = useState(JSON.parse(localStorage.getItem("darkMode")) || false)
+  const modeClasses = dark ? "bg dark text-white min-vh-100" : "bg-light"
   return (
     <div className="container my-4">
       <h1 className="text-center">ToDos App</h1>
---
+++ src/components/DarkMode.js
@@ -0,0 +1,13 @@
+
+
+const DarkMode = () => {
+
+  return[
+    <section>
+      <div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" id="activate" />
+        <label class="form-check-label" for="activate"> Mode Sombre </label>
+      </div>
+    </section>
+  ]
+}
